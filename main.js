2fd4090388837c5ad63e32c0db1ad5f724b043d2
mish01921 <misho191524@gmail.com>
2022-07-25 12:20:30 +0400

    new commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function SendInfo(){
+    alert("Thank you for your message" + " " + document.getElementById('full_name').value + " "+ "jan");
+}
+
+function LetsGo(){
+    document.getElementById('aboutUs-title').innerHTML ="Welcome to our team";
+}
